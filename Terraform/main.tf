0d7724ae0be02a98b0c2d7fe584edd942b9ce75c
Valentin Horacek <valentin.horacek@gmail.com>
2025-07-15 22:05:44 +0200

    Terraform - remote backend

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,19 @@
 terraform {
+  cloud { 
+    organization = "valentin-horacek-devops" 
+
+    workspaces { 
+      name = "linode-devops" 
+    } 
+  } 
+  
   required_providers {
     linode = {
       source  = "linode/linode"
       version = "~> 3.0.0"
     }
   }
+    
 }
 
 provider "linode" {
