4f60500a9243b6cecc81d0149a476c88ca71f0cc
Valentin Horacek <valentin.horacek@gmail.com>
2025-10-27 20:54:12 +0100

    ﻿Terraform - Clouflare update

--- Terraform/cloudlfare.tf
+++ Terraform/cloudlfare.tf
@@ -5,7 +5,7 @@ provider "cloudflare" {
 resource "cloudflare_dns_record" "jenkins" {
   zone_id = var.cloudflare_zone_id
   name    = "jenkins"
-  content = one(linode_instance.devops_server.ipv4) # reference Linode IP dynamically
+  content   = one(linode_instance.devops_server.ipv4)  # reference Linode IP dynamically
   type    = "A"
   ttl     = 300
   proxied = false
